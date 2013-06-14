7b0542ed2b5c08fbcb6462c92235e97b5a421520
Tchaj <Tchaj@Tchaj-PC>
2013-06-14 16:40:40 +0200

    upload .gitignore

---
+++ temp/cache/_Nette.FileTemplate/_templates._layout.latte-07e1b2b5db127f78d0ea017d5f4fa22c.php
@@ -0,0 +1,92 @@
+<?php //netteCache[01]000381a:2:{s:4:"time";s:21:"0.23168400 1371168383";s:9:"callbacks";a:2:{i:0;a:3:{i:0;a:2:{i:0;s:19:"Nette\Caching\Cache";i:1;s:9:"checkFile";}i:1;s:59:"C:\xampp\htdocs\SupportBeta_1.0\app\templates\@layout.latte";i:2;i:1371167406;}i:1;a:3:{i:0;a:2:{i:0;s:19:"Nette\Caching\Cache";i:1;s:10:"checkConst";}i:1;s:25:"Nette\Framework::REVISION";i:2;s:30:"c0332ac released on 2013-03-08";}}}?><?php
+
+// source file: C:\xampp\htdocs\SupportBeta_1.0\app\templates\@layout.latte
+
+?><?php
+// prolog Nette\Latte\Macros\CoreMacros
+list($_l, $_g) = Nette\Latte\Macros\CoreMacros::initRuntime($template, 'dcyq2s56s4')
+;
+// prolog Nette\Latte\Macros\UIMacros
+//
+// block content
+//
+if (!function_exists($_l->blocks['content'][] = '_lb90931d11b1_content')) { function _lb90931d11b1_content($_l, $_args) { extract($_args)
+?><div id="banner">
+<?php call_user_func(reset($_l->blocks['title']), $_l, get_defined_vars())  ?>
+</div>
+
+<div id="content">
+	<h2>Kontaktovat Podporu</h2>
+Tohle je test.
+	
+
+	
+
+        <footer>&copy; 2013 Laroxion | Verze Aplikace 1.0 | Vytvořil <font color="green" >Martin Horáček<font></footer>
+</div>
+<?php
+}}
+
+//
+// block title
+//
+if (!function_exists($_l->blocks['title'][] = '_lb3fd8d183a7_title')) { function _lb3fd8d183a7_title($_l, $_args) { extract($_args)
+?>	<h1>Laroxion - Podpora</h1>
+<?php
+}}
+
+//
+// block head
+//
+if (!function_exists($_l->blocks['head'][] = '_lb9205fc9e6f_head')) { function _lb9205fc9e6f_head($_l, $_args) { extract($_args)
+?><style>
+html { overflow-y: scroll; }
+body { font: 14px/1.65 Verdana, "Geneva CE", lucida, sans-serif; background: #3484d2; color: #333; margin: 38px auto; max-width: 940px; min-width: 770px; }
+
+h1, h2 { font: normal 150%/1.3 Georgia, "New York CE", utopia, serif; color: #1e5eb6; -webkit-text-stroke: 1px rgba(0,0,0,0); }
+
+img { border: none; }
+
+a { color: #006aeb; padding: 3px 1px; }
+
+a:hover, a:active, a:focus { background-color: #006aeb; text-decoration: none; color: white; }
+
+#banner { border-radius: 12px 12px 0 0; background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAB5CAMAAADPursXAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAGBQTFRFD1CRDkqFDTlmDkF1D06NDT1tDTNZDk2KEFWaDTZgDkiCDTtpDT5wDkZ/DTBVEFacEFOWD1KUDTRcDTFWDkV9DkR7DkN4DkByDTVeDC9TDThjDTxrDkeADkuIDTRbDC9SbsUaggAAAEdJREFUeNqkwYURgAAQA7DH3d3335LSKyxAYpf9vWCpnYbf01qcOdFVXc14w4BznNTjkQfsscAdU3b4wIh9fDVYc4zV8xZgAAYaCMI6vPgLAAAAAElFTkSuQmCC); }
+#banner h1 { color: white; font-size: 50px; line-height: 121px; margin: 0; padding-left: 40px; background: url(http://files.nette.org/sandbox/logo.png) no-repeat 95%; text-shadow: 1px 1px 0 rgba(0, 0, 0, .9); }
+
+#content { background: white; border: 1px solid #eff4f7; border-radius: 0 0 12px 12px; padding: 10px 40px; }
+#content > h2 { font-size: 130%; color: #666; clear: both; padding: 1.2em 0; margin: 0; }
+
+h2 span { color: #87A7D5; }
+h2 a { text-decoration: none; background: transparent; }
+
+footer { font-size: 70%; padding: 1em 0; color: gray; }
+
+</style>
+<?php
+}}
+
+//
+// end of blocks
+//
+
+// template extending and snippets support
+
+$_l->extends = empty($template->_extended) && isset($_control) && $_control instanceof Nette\Application\UI\Presenter ? $_control->findLayoutTemplateFile() : NULL; $template->_extended = $_extended = TRUE;
+
+
+if ($_l->extends) {
+	ob_start();
+
+} elseif (!empty($_control->snippetMode)) {
+	return Nette\Latte\Macros\UIMacros::renderSnippets($_control, $_l, get_defined_vars());
+}
+
+//
+// main template
+//
+if ($_l->extends) { ob_end_clean(); return Nette\Latte\Macros\CoreMacros::includeTemplate($_l->extends, get_defined_vars(), $template)->render(); }
+call_user_func(reset($_l->blocks['content']), $_l, get_defined_vars())  ?>
+
+
+<?php call_user_func(reset($_l->blocks['head']), $_l, get_defined_vars()) ; 
